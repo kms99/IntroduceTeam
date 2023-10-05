dedf7f7788463d436816358a479c242478a4096c
Byeong Taek <respect21047436@gmail.com>
2023-10-05 13:12:37 +0900

    포스팅 카드 토글기능, 데이터연동, 이미지파일 firestorage 연동, 페이지 이동 querystring추가, css수정 등

--- posting.js
+++ posting.js
@@ -1,42 +1,159 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js";
+import {
+getAuth,
+signInWithPopup,
+GoogleAuthProvider,
+signOut,
+} from "https://www.gstatic.com/firebasejs/10.3.1/firebase-auth.js";
+import {
+collection,
+addDoc,
+} from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js";
+import { getDocs } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-firestore.js";
+import {
+getDownloadURL,
+getStorage,
+ref,
+uploadBytes,
+} from "https://www.gstatic.com/firebasejs/10.3.1/firebase-storage.js";
+
+// Firebase 구성 정보 설정
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+apiKey: "AIzaSyBCRtnQasPnOrIwbSAfN2gQdWYqrkiwMzg",
+authDomain: "sparta-c48a5.firebaseapp.com",
+projectId: "sparta-c48a5",
+storageBucket: "sparta-c48a5.appspot.com",
+messagingSenderId: "823259035177",
+appId: "1:823259035177:web:2efbea644a5e0710a30e38",
+measurementId: "G-P79TQTLFWQ",
+};
+// Firebase 인스턴스 초기화
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+//firebase-storage
+const storage = getStorage(app);
+//루트 > images
+const storageRef = ref(storage);
+const imagesRef = ref(storageRef, "images");
+
+// 파이어스토어 데이터추가
+$("#posting-btn").click(async function () {
+let name = $("#name").val();
+let living = $("#living").val();
+let MBTI = $("#MBTI").val();
+let blog = $("#blog").val();
+let git = $("#git").val();
+let turn = $("#turn").val();
+let goal = $("#goal").val();
+let firstSalary = $("#firstSalary").val();
+let Advantages = $("#Advantages").val();
+let collabo = $("#collabo").val();
+
+let doc = {
+name: name,
+living: living,
+MBTI: MBTI,
+blog: blog,
+git: git,
+turn: turn,
+goal: goal,
+firstSalary: firstSalary,
+Advantages: Advantages,
+collabo: collabo,
+};
+
+
+async function add(doc){
+await addDoc(collection(db, "member-cards"), doc);
+alert("저장완료!");
+window.location.reload();
+}
+
+// 이미지 firebase-storage 저장
+const image = document.getElementById("images").files[0];
+
+//firebase-storage
+const storageRef = ref(imagesRef, image.name);
+uploadBytes(storageRef, image).then((snapshot) => {
+getDownloadURL(ref(storageRef)).then((url) => {
+doc["imagePath"] = url;
+if (
+!image ||
+!name ||
+!living ||
+!MBTI ||
+!blog ||
+!git ||
+!goal ||
+!firstSalary ||
+!Advantages ||
+!collabo
+) {
+alert("내용을 모두 입력해주세요.");
+} else {
+add(doc);
+}
+});
+});
+});
+
+
+// 파베 데이터 가져오기
+let docs = await getDocs(collection(db, "member-cards"));
+docs.forEach((doc) => {
+let row = doc.data();
+
+let imagePath = row["imagePath"];
+let name = row["name"];
+let goal = row["goal"];
+
+let temp_html = `
+<div class="flip">
+<div class="card" data-code-id="${doc.id}">
+<div class="front">
+<img
+src="${imagePath}" />
+</div>
+<div class="back">
+<div class="card-name">${name}</div>
+<div class="card-contents">"${goal}"</div>
+</div>
+</div>
+</div>`;
+$("#card").append(temp_html);
+});
+
 function openclose() {
-    $('#postingbox').toggle();
+$("#postingbox").toggle();
 }
+// 토글 버튼과 숨겨진 콘텐츠를 가져오기
+const toggleButton = document.getElementById("toggleButton");
+const hiddenContent = document.getElementById("hiddenContent");
+const closeButton = document.getElementById("closeButton");
 
-function makeCard() {
-    let images = $('#images').val();
-    let name = $('#name').val();
-    let living = $('#living').val();
-    let MBTI = $('#MBTI').val();
-    let blog = $('#blog').val();
-    let git = $('#git').val();
-    let goal = $('#goal').val();
-    let firstSalary = $('#firstSalary').val();
-    let Advantages = $('#Advantages').val();
-    let collabo = $('#collabo').val();
+// 토글 버튼 클릭 시 이벤트 처리
+toggleButton.addEventListener("click", function () {
+// 토글 버튼 숨기기
+toggleButton.style.display = "none";
+// 숨겨진 콘텐츠 보이기
+hiddenContent.style.display = "block";
+});
 
-    let temp_html = `            
-    <div class="col">
-        <div class="card h-100">
-            <img src="${images}"
-                class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-name">${name}</h5>
-                <p class="card-living">${living}</p>
-                <p class="card-MBTI">${MBTI}</p>
-                <p class="card-goal">${goal}</p>
-                <p class="card-firstSalary">${firstSalary}</p>
-                <p class="card-Advantages">${Advantages}</p>
-                <p class="card-collabo">${collabo}</p>
-            </div>
-            <div class="card-footer">
-                <small class="text-body-blog">${blog}</small>
-                <small class="text-body-git">${git}</small>
-            </div>
-        </div>
-    </div>`;
-    $('#card').append(temp_html);
-}
+// 닫기 버튼 클릭 시 이벤트 처리
+closeButton.addEventListener("click", function () {
+// 숨겨진 콘텐츠 숨기기
+hiddenContent.style.display = "none";
+// 토글 버튼 보이기
+toggleButton.style.display = "block";
+});
 
-
-document.getElementById('posting-btn').addEventListener('click',makeCard)
-document.getElementById('posting-box-open-btn').addEventListener('click',openclose)
+document.querySelectorAll(".card").forEach((item) => {
+item.addEventListener("click", function () {
+window.location.href = `./detail.html?${item.dataset.codeId}`;
+});
+});
